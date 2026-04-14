8767a8c51623f974abb9a35dd20587aab8e3e270
roji20112 <orangref16@gmail.com>
2026-04-14 16:47:09 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-zz
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = "YOUR_BOT_TOKEN";
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+  const userId = msg.from.id;
+
+  const link = `https://yoursite.com/?id=${userId}`;
+
+  bot.sendMessage(msg.chat.id, `
+🔗 هذا رابطك الخاص:
+
+${link}
+
+📌 أي حاجة تصير في الرابط ترجع لك وحدك
+  `);
+});
